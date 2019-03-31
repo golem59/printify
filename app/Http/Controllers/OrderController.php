45988a511dcffa6c46347c73e16f97efba61cffe
Ivan Svetlakov <svetlakoviv@gmail.com>
2019-04-01 02:01:10 +0300

    some refactoring

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Order;
 use App\OrderItem;
 use App\Http\Requests\OrderRequest;
-use App\Product;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
@@ -13,6 +12,10 @@ use Illuminate\Support\Facades\DB;
 class OrderController extends Controller
 {
     const MAX_API_CALLING_BY_COUNTRY = 3;
+    const ORDER_IS_EMPTY = 'order is empty!';
+    const TOO_MANY_ATTEMPTS = 'too many attempts from your country!';
+    const ERROR_TOTAL_PRICE_IS_LESS_THAN_MINIMUM = 'total price is less than minimum, order was deactivated!';
+    const MIN_PRICE = 10;
     /**
      * Display a listing of the resource.
      *
@@ -34,54 +37,88 @@ class OrderController extends Controller
         $countryCode = $request->get('country_code');
         $products = $request->get('products');
         if(!count($products)){
-            return response()->json(['error' => 'order is empty!']);
+            return response()->json(['error' => self::ORDER_IS_EMPTY]);
         }
         $info = json_encode($products);
 
-        //for case when we have same products multiple times
+        $products = $this->sumProductsQuantity($products);
+
+        if(!$this->isThrottlingOk($countryCode)){
+            return response()->json(['error' => self::TOO_MANY_ATTEMPTS]);
+        }
+
+        $order = $this->createOrder($countryCode,$info);
+        $this->createOrderItems($products, $order->id);
+        $sum = OrderItem::totalPriceForOrder($order->id);
+
+        if($sum < self::MIN_PRICE){
+            $order->is_active = false;
+            $order->save();
+            return response()->json(['error' => self::ERROR_TOTAL_PRICE_IS_LESS_THAN_MINIMUM]);
+        }
+        return response()->json(['total_price' => $sum]);
+    }
+
+    /**
+     * sum quantity for same products
+     * @param array $products
+     * @return array
+     */
+    protected function sumProductsQuantity(array $products)
+    {
         $preparedProducts = [];
         foreach ($products as $product){
             $preparedProducts[$product['id']] =  $preparedProducts[$product['id']]??0;
             $preparedProducts[$product['id']] += $product['quantity'];
         }
 
-        //limit number of calling by country
-        $counter = Cache::get($countryCode, 0);
-        $counter++;
-        if($counter > self::MAX_API_CALLING_BY_COUNTRY){
-            return response()->json(['error' => 'too many attempts from your country!']);
+        return $preparedProducts;
+    }
+
+    /**
+     * @param array $products
+     * @param int $orderId
+     */
+    protected function createOrderItems(array $products, int $orderId)
+    {
+        foreach ($products as $key => $value){
+            OrderItem::query()->create([
+                'order_id' => $orderId,
+                'product_id' => $key,
+                'quantity' => $value
+            ]);
         }
+    }
 
-        $expiresAt = Carbon::now()->addSeconds(1);
-        Cache::store('file')->put($countryCode, $counter, $expiresAt);
-
-        //create an order
+    /**
+     * @param string $countryCode
+     * @param string $info
+     * @return $this|\Illuminate\Database\Eloquent\Model
+     */
+    protected function createOrder(string $countryCode, string $info)
+    {
         $orderArray = [
             'country_code' => $countryCode,
             'info' => $info
         ];
-        $order = Order::query()->create($orderArray);
 
-        //create order Items
-        foreach ($products as $product){
-            OrderItem::query()->create([
-                'order_id' => $order->id,
-                'product_id' => $product['id'],
-                'quantity' => $product['quantity']
-            ]);
+        return Order::query()->create($orderArray);
+    }
+
+    /**
+     * @param $country
+     * @return bool
+     */
+    protected function isThrottlingOk($country)
+    {
+        $counter = Cache::get($country, 0);
+        $counter++;
+        if($counter > self::MAX_API_CALLING_BY_COUNTRY){
+            return false;
         }
 
-        //calculate total price for output
-        $sum = OrderItem::query()
-            ->leftJoin('Products as p', 'p.id','=','product_id')
-            ->where('order_id',$order->id)
-        ->sum(DB::raw('quantity * price'));//cutting edges with raw
-
-        if($sum<10){
-            $order->is_active = false;
-            $order->save();
-            return response()->json(['error' => 'total price is less than minimum, order was deactivated!']);
-        }
-        return response()->json(['total_price' => $sum]);
+        $expiresAt = Carbon::now()->addSeconds(1);
+        Cache::store('file')->put($country, $counter, $expiresAt);
+        return true;
     }
 }
--- app/OrderItem.php
+++ app/OrderItem.php
@@ -3,10 +3,24 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class OrderItem extends Model
 {
     protected $fillable = ['product_id', 'order_id', 'quantity'];
 
     protected $hidden = ['created_at', 'updated_at'];
+
+    /**
+     * calculate total price for given order
+     * @param int $orderId
+     * @return mixed
+     */
+    public static function totalPriceForOrder(int $orderId)
+    {
+        return self::query()
+            ->leftJoin('Products as p', 'p.id','=','product_id')
+            ->where('order_id',$orderId)
+            ->sum(DB::raw('quantity * price'));
+    }
 }
